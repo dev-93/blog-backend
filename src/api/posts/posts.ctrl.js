8d69a88442d97164a4989cbb3c1382aff57ad0bd
Kim taenam <cjb940103@gmail.com>
2021-07-25 23:45:38 +0900

    블로그 포스트 작성 수정

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,104 +1,25 @@
-let postId = 1;
+import Post from "../../models/post";
 
-// 초기 데이터
-const posts = [
-    {
-        id: 1,
-        title: "제목",
-        body: "내용",
+export const write = async ctx => {
+    const {title, body, tags} = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+
+    try {
+        await post.save();
+        ctx.body = post;
+    }  catch(e) {
+        ctx.throw(500, e);
     }
-];
-
-// 포스트 작성
-// POST /api/posts
-
-export const write = ctx => {
-    const {title, body} = ctx.request.body;
-    postId += 1;
-    const post = {id: postId, title, body};
-    posts.push(post);
-    ctx.body = post;
 };
 
-// 포스트 목록 조회
-// GET /api/posts
+export const list = ctx => {}
 
-export const list = ctx => {
-    ctx.body = posts;
-}
+export const read = ctx => {};
 
-// 특정 포스트 조회
-// GET /api/posts/:id
+export const remove = ctx => {}
 
-export const read = ctx => {
-    const { id } = ctx.params;
-    const post = posts.find(p => p.id.toString() === id);
-    if(!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: "포스트가 존재하지 않습니다.",
-        }
-        return;
-    }
-    ctx.body = post;
-};
-
-// 특정 포스트 제거
-// DELETE /api/posts/:id
-
-export const remove = ctx => {
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: "포스트가 존재하지 않습니다.",
-        }
-        return;
-    }
-    posts.slice(index, 1);
-    ctx.status = 204; 
-}
-
-// 포스트 수정
-// PUT /api/posts/:id
-
-export const replace = ctx => {
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: "포스트가 존재하지 않습니다.",
-        }
-        return;
-    }
-    posts[index] = {
-        id,
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
-};
-
-// 포스트 수정 (특정 필드 변경)
-// PATCH /api/posts/:id
-
-export const update = ctx => {
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: "포스트가 존재하지 않습니다.",
-        }
-        return;
-    }
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
-};
+export const update = ctx => {};
