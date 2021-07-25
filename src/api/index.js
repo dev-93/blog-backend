9419b20e787cfe97f342bc9b12bfb6b19adca03e
Kim taenam <cjb940103@gmail.com>
2021-07-25 14:24:42 +0900

    라우트 모듈화

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 const Router = require("koa-router");
+const posts = require("./posts");
+
 const api = new Router();
 
 // 라우터 설정
-router.get('/test', ctx => {
-    ctx.body = "test 성공";
-});
+api.use('/posts', posts.routes());
 
 module.exports = api;
