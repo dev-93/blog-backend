bcd36abca057ad425301e3ae910508cdbb2cab89
Kim Taenam <cjb940103@gmail.com>
2021-08-25 15:40:32 +0900

    deta tst

--- src/main.js
+++ src/main.js
@@ -23,18 +23,14 @@ const router = new Router();
 
 app.use(cors());
 
-app.get('/', async (req, res) => {
-    res.send('Hello World')
-});
+//라우터 설정
+router.use('/api', api.routes());
 
-// //라우터 설정
-// router.use('/api', api.routes());
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+app.use(jwtMiddleware);
 
-// // 라우터 적용 전에 bodyParser 적용
-// app.use(bodyParser());
-// app.use(jwtMiddleware);
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
-// // app 인스턴스에 라우터 적용
-// app.use(router.routes()).use(router.allowedMethods());
-
-module.exports = app; 
+export default app;
